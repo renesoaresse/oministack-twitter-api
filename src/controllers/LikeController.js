c7b601e34dd64122a74d9baa9163097b9ebac019
Renê Soares <reenerochase@gmail.com>
2019-10-24 11:35:02 -0300

    feature(feature-like-tweet) Implemented the logic

--- src/controllers/LikeController.js
+++ src/controllers/LikeController.js
@@ -1,7 +1,15 @@
 const Tweet = require('../models/Tweet')
 
 class LikeController {
-  async store (req, res) {}
+  async store (req, res) {
+    const tweet = await Tweet.findById(req.params.id)
+
+    tweet.set({ likes: tweet.likes++ })
+
+    await tweet.save()
+
+    return res.json(tweet)
+  }
 }
 
 module.exports = new LikeController()
