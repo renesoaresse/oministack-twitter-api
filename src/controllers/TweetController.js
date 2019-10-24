0cb111224d79aa920dd5835d74ac5d57f98da19c
Renê Soares <reenerochase@gmail.com>
2019-10-24 08:49:17 -0300

    feat(controller) Application control created with the methods index and store.

---
+++ src/controllers/TweetController.js
@@ -0,0 +1,16 @@
+const Tweet = require('../models/Tweet')
+
+class TweetController {
+  async index (req, res) {
+    const tweets = await Tweet.find({}).sort('-createdAt')
+
+    return res.json(tweets)
+  }
+
+  async store (req, res) {
+    const tweet = await Tweet.create(req.body)
+    return res.json(tweet)
+  }
+}
+
+module.exports = new TweetController()
