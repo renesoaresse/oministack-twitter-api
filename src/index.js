95fe3f6483f8f84801b1bde2fa4caa61875438a7
Renê Soares <reenerochase@gmail.com>
2019-10-24 08:00:31 -0300

    feat(config-database) Install mongoose.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-const express = require("express");
+const express = require('express')
+const mongoose = require('mongoose')
 
-const app = express();
+const app = express()
+
+mongodb://twitter:root@localhost:8081/twitter
