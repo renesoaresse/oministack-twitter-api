45f9d134e075d0579d23f908044b3465d49afdff
Renê Soares <reenerochase@gmail.com>
2019-10-24 08:06:22 -0300

    feat(plugins) Install nodemon.

--- src/index.js
+++ src/index.js
@@ -3,4 +3,14 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-mongodb://twitter:root@localhost:8081/twitter
+mongoose.connect('mongodb://twitter:root@localhost:8081/twitter', {
+  useNewUrlParser: true
+})
+
+app.get('/', (req, res) => {
+  return res.send('Init application')
+})
+
+app.listen(3000, () => {
+  console.log('Server port 3000')
+})
